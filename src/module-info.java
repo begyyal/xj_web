4974afa5a5cc8585927afc67fdcba37f5dfa48cd
begyyal <begyyal@gmail.com>
2022-03-23 14:29:16 +0900

    W[»

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module begyyal.xj_web {
+    requires begyyal.xj_commons;
+
+    exports begyyal.web;
+    exports begyyal.web.constant;
+    exports begyyal.web.html;
+    exports begyyal.web.html.constant;
+    exports begyyal.web.html.object;
+}
